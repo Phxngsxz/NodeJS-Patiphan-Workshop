a53088cfc558388aa62597768baa2a300804428a
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-19 14:40:34 +0700

    setup database

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs-patiphan-workshop'
+})
+
+db.connect((err) => {
+    if(err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+
+    console.log('Connected to MySQL database successfully.');
+})
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('home', { title : 'Home'});
+    const sql = "SELECT * FROM users";
+
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.render('home', { 
+            title : 'Home',
+            users: results
+        });
+    })
 });
 
 app.get('/about', (req, res) => {
